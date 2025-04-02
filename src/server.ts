7135289111397c0ce259658790200f94ad116b38
abishkar rai <raiabishkar0.5@gmail.com>
2025-04-02 11:25:16 +1100

    intial folder setup

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+const app = express();
+dotenv.config()
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+
