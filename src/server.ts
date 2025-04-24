dc4aa33cb230ca496524121534ae71b7cb137e30
abishkar rai <raiabishkar0.5@gmail.com>
2025-04-24 10:47:35 +1000

    setup dbconfig for remote db

--- src/server.ts
+++ src/server.ts
@@ -1,64 +1,70 @@
-import dotenv from 'dotenv';
-import express,{Express} from 'express';
-import multer from 'multer';
-import path from 'path';
-import cors from 'cors';
-import fs from 'fs';
-import helmet from 'helmet'
+import dotenv from "dotenv";
+import express, { Express } from "express";
+import multer from "multer";
+import path from "path";
+import cors from "cors";
+import fs from "fs";
+import helmet from "helmet";
 
-const app:Express = express();
-dotenv.config()
+const app: Express = express();
+dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-//middleware 
+//dbc connection
+import { connectDB } from "./config/dbConfig";
+connectDB();
 
-app.use(helmet())
+//middleware
+
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
 const storage = multer.diskStorage({
-    destination:(req, file, cd)=>{
-        cd(null, './uploads/');
-    },
+  destination: (req, file, cd) => {
+    cd(null, "./uploads/");
+  },
 
-    filename:(req, file, cd) =>{
-        const uniqueSuffix = Date.now()+ '-' + Math.round(Math.random() * 1E9);
-        cd(null, uniqueSuffix + path.extname(file.originalname));
-    }
-})
+  filename: (req, file, cd) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cd(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
 
-const fileFilter = (req: Express.Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-    const allowedFileTypes = ['.pdf', '.doc', '.docx'];
-    const ext = path.extname(file.originalname).toLowerCase();
-    
-    if (allowedFileTypes.includes(ext)) {
-      cb(null, true);
-    } else {
-      cb(new Error('Invalid file type. Only PDF, DOC, and DOCX files are allowed.'));
-    }
-  };
-  
+const fileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
+  const allowedFileTypes = [".pdf", ".doc", ".docx"];
+  const ext = path.extname(file.originalname).toLowerCase();
+
+  if (allowedFileTypes.includes(ext)) {
+    cb(null, true);
+  } else {
+    cb(
+      new Error("Invalid file type. Only PDF, DOC, and DOCX files are allowed.")
+    );
+  }
+};
+
 export const upload = multer({
-    storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
-    fileFilter
-  });
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  fileFilter,
+});
 
-const uploadsDir = path.join(__dirname, '../uploads');
-if(!fs.existsSync(uploadsDir)){
-  fs.mkdirSync(uploadsDir)
+const uploadsDir = path.join(__dirname, "../uploads");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
 }
 
+import resumeRoutes from "./route/resumeRoute";
 
-
-import resumeRoutes from './route/resumeRoute'
-// http://localhost:8000/api/v1/resumes
-
-// route 
-app.use('/api/v1/resumes/upload', resumeRoutes)
-
+// route
+app.use("/api/v1/resumes/upload", resumeRoutes);
 
 app.use("/", (req, res, next) => {
   const error = {
@@ -76,8 +82,6 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
-
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
